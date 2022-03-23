9de704790e3940b89005b8afef17d9630b3bc30e
EDGlio <101647244+EDGlio@users.noreply.github.com>
2022-03-23 11:47:39 +0100

    Access multiple files in a directory
    Made it so you can access all files in directory
    Creates a new directory, where each .txt file is a list that represents its corresponding png

--- main.py
+++ main.py
@@ -1,16 +1,25 @@
 # Import the necessary libraries
 from PIL import Image
 from numpy import asarray
-  
+import os
+
+directory = 'squares'
+images = os.listdir(directory+'/')
+print(images)
+
+newdir = os.mkdir('list'+directory+'/')
 
 # load the image and convert into 
 # numpy array
-img = Image.open('insert file here.png')
-numpydata = asarray(img)
-  
-# data
-data = []
-for i in numpydata:
-  for j in i:
-    data.append(j[1]/255)
-print(data)
+for nom in images:
+  img = Image.open(f'{directory}/{nom}')
+  numpydata = asarray(img)
+
+  completeName = os.path.join(f'list{directory}', nom)
+  # data
+  data = []
+  for i in numpydata:
+    for j in i:
+      data.append(j[1]/255)
+  with open(f'{completeName[:-4]}.txt', 'w') as file:
+    file.write(str(data))
